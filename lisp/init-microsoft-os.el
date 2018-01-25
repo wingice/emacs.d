99c361cd963b9f89fd2c6e2f743efd3e889ae395
Li <i348151@c02w13p8htd6.dhcp.pvgl.sap.corp>
2018-01-25 13:45:50 +0800

    Add local-notification-tpm and notification-implementation-on-macosx

--- lisp/init-microsoft-os.el
+++ lisp/init-microsoft-os.el
@@ -10,49 +10,11 @@
 
   (when window-system (set-frame-size (selected-frame) 132 36))
 
-  (setq pdf-info-epdfinfo-program "C:/Tools/msys64/mingw64/bin/epdfinfo.exe")
-
-  
+  ;;  (setq pdf-info-epdfinfo-program "C:/Tools/msys64/mingw64/bin/epdfinfo.exe")
   ;;------------org-mode and appt settings
   (setq org-agenda-files '("c:/workspace/orgagenda/agenda.org" "c:/workspace/orgagenda/remember_notes.org"))
-
   (setq org-default-notes-file "c:/workspace/orgagenda/remember_notes.org")
   (setq default-directory "C:/workspace/working/")
-
-  ;; appt and reminder
-  (require 'appt)
-  (setq org-agenda-include-diary t)
-  (setq appt-time-msg-list nil)
-
-  ;; the appointment notification facility
-  (setq
-   appt-message-warning-time 3
-   appt-display-mode-line t ;; show in the modeline
-   display-time-default-load-average nil
-   appt-display-format 'window) ;; use our func
-  
-  (appt-activate 1) ;; active appt (appointment notification)
-  (display-time) ;; time display is required for this...
-
-
-
-  (setq org-timer-default-timer 25)
-  ;;Modify the org-clock-in so that a timer is started with the default
-  ;;value except if a timer is already started :
-  (add-hook 'org-clock-in-hook '(lambda ()
-				  (if (not org-timer-current-timer)
-				      (org-timer-set-timer '(16)))))
-  (add-hook 'org-clock-out-hook '(lambda ()
-				   (setq org-mode-line-string nil)
-				   (org-timer-cancel-timer)
-				   ))
-
-  (add-hook 'org-timer-done-hook '(lambda()
-				    (growl-popup "Congratulations! You Finished a Pomodoro Task!")))
-
-  (defun my-after-load-org ()
-    (add-to-list 'org-modules 'org-timer))
-  (eval-after-load "org" '(my-after-load-org))
 
   (defun growl-popup(msg)
     (interactive)
@@ -68,25 +30,6 @@
     (select-frame-set-input-focus (selected-frame))
     (growl-popup appt-msg)
     )
-
-  ;; update appt each time agenda opened
-  (add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt)
-  (setq appt-disp-window-function (function growl-appt))
-
-
-  (add-hook 'org-mode-hook
-	    (lambda()
-	      (add-hook 'before-save-hook 'org-agenda-to-appt t t)
-	      ))
-
-  ;; Resume clocking tasks when emacs is restarted
-  (org-clock-persistence-insinuate)
-  ;; Do not prompt to resume an active clock
-  (setq org-clock-persist-query-resume nil)
-  ;; Save the running clock and all clock history when exiting Emacs, load it on startup
-  (setq org-clock-persist t)
-
-  
   )
 
 (provide 'init-microsoft-os)
--- readme.md
+++ readme.md
@@ -12,3 +12,10 @@ Steps
 
 You are ready to go, enjoy!
 
+
+Further customization
+---------------------
+If you want to change, just change the lisp file freely.
+If you want to add some local configuration which only applied to local machine, you can add
+a local-conf-body.el in lisp sub folder. It will be automatically loaded. You can set
+working-directory, agenda-file location etc on this file.
