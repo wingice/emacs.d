25c4bdffb077ce8fe3b92366898d938d5f1ad601
William.Lee <wingicelee@hotmail.com>
2019-12-20 17:41:46 +0800

    not open a new frame when using ediff

--- lisp/init-display.el
+++ lisp/init-display.el
@@ -4,7 +4,6 @@
 
 (column-number-mode t)
 (setq linum-format "%5d ")
-(setq ediff-split-window-function 'split-window-horizontally)
 (setq sgml-basic-offset 4)
 
 ;;disable splash screen and startup message
@@ -49,6 +48,8 @@
 (add-hook 'prog-mode-hook 'hs-minor-mode)
 
 (setq ediff-diff-options "-w")
+(setq ediff-split-window-function 'split-window-horizontally)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 (defvar after-load-theme-hook nil
     "Hook run after a color theme is loaded using `load-theme'.")
