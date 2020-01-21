790bac8b06713eedbb9dfa682127b46d16608bbd
William.Lee <wingicelee@hotmail.com>
2020-01-21 10:05:45 +0800

    Rewrite the notification-method with Powershell

--- lisp/init-microsoft-os.el
+++ lisp/init-microsoft-os.el
@@ -12,25 +12,24 @@
   (when window-system (set-frame-size (selected-frame) 132 36))
 
   ;;  (setq pdf-info-epdfinfo-program "C:/Tools/msys64/mingw64/bin/epdfinfo.exe")
-  ;;------------org-mode and appt settings
-  (setq org-agenda-files '("c:/workspace/orgagenda/agenda.org" "c:/workspace/orgagenda/remember_notes.org"))
-  (setq org-default-notes-file "c:/workspace/orgagenda/remember_notes.org")
-  (setq default-directory "C:/workspace/working/")
 
-  (defun growl-popup(msg)
+  (defun powershell-toast(msg)    ;; Install-Module -Name BurntToast   [@Powershell Administrator mode]
     (interactive)
-    (start-process "remind" nil "C:/workspace/working/SmartReminders/SmartReminder/ReminderClient/bin/Debug/ReminderClient.exe" msg))
-
-  (defun growltest()
+    (start-process "remind"  nil "powershell.exe" "-Command" "New-BurntToastNotification" (concat "-Text GTD," "\"" msg "\"")))
+   
+  (defun toast-test()
     (interactive)
-    (growl-appt 1 2 "Hello, Test successfully!")
-    )
+    (powershell-appt 1 2 "Hello World!"))
 
-  (defun growl-appt(min-to-app new-time appt-msg)
+  (defun powershell-appt(min-to-app new-time appt-msg)
     (interactive)
     (select-frame-set-input-focus (selected-frame))
-    (growl-popup appt-msg)
-    )
+    (powershell-toast appt-msg))
+
+
+  (defun popup-notification(title msg)
+    (interactive)
+    (powershell-toast msg))
   )
 
 (provide 'init-microsoft-os)
--- lisp/local-conf-tpl.el
+++ lisp/local-conf-tpl.el
@@ -14,3 +14,6 @@
 ;;(setq find-program "c:\\Users\\i348151\\scoop\\shims\\find.exe")
 ;;(setq find-ls-option '("-print0 | xargs -0 ls -alhd" . ""))
 
+;;  (setq org-agenda-files '("c:/workspace/orgagenda/agenda.org" "c:/workspace/orgagenda/remember_notes.org"))
+;;  (setq org-default-notes-file "c:/workspace/orgagenda/remember_notes.org")
+
