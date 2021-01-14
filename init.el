1f2b95fa5aae7d2ab53d84154d2eaa0afb3a5e47
William <wingicelee@hotmail.com>
2021-01-14 19:33:48 +0800

    Add inf-ruby robe to init.el

--- init.el
+++ init.el
@@ -64,4 +64,4 @@
  '(custom-safe-themes
    '("41c8c11f649ba2832347fe16fe85cf66dafe5213ff4d659182e25378f9cfc183" "39ecc1e45ef87d610d0a8296701327010239ab70d2fc22d8e6254a30c80d497e" default))
  '(package-selected-packages
-   '(goto-chg transpose-frame helm org-roam smart-jump yaml-mode chruby projectile-rails projectile-ripgrep markdown-mode nimbus-theme helm-projectile json-mode web-mode projectile yasnippet rinari flymake-ruby company flx-ido)))
+   '(inf-ruby robe goto-chg transpose-frame helm org-roam smart-jump yaml-mode chruby projectile-rails projectile-ripgrep markdown-mode nimbus-theme helm-projectile json-mode web-mode projectile yasnippet rinari flymake-ruby company flx-ido)))
--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,8 +1,9 @@
 (yas-global-mode 1)
-(setq yas-snippet-dirs (cons "~/.emacs.d/conf_data/snippets" yas-snippet-dirs))
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/conf_data/snippets")
 (setq yas-prompt-functions '(yas-x-prompt yas-dropdown-prompt))
 
 (add-hook 'term-mode-hook (lambda()
 			    (setq yas-dont-activate t)))
+(yas-reload-all)
 
 (provide 'init-yasnippet)
