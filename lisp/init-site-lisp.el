8ce20ddf2c9037ac127d039aade3128b2299baff
William.Lee <wingicelee@hotmail.com>
2023-02-20 09:15:46 +0800

    better handle the server-running-p predictate

--- lisp/init-site-lisp.el
+++ lisp/init-site-lisp.el
@@ -8,8 +8,22 @@
 
 (defun server-ensure-safe-dir (dir) "Noop" t)
 (setq server-socket-dir (concat emacs-dir "server"))
-(unless (server-running-p)
-  (server-start))
+(cond
+ ((eq (server-running-p) nil)
+  (progn
+    (message "Starting new server.")
+    (server-start)))
+ ((eq (server-running-p) :other)
+  (let ((my-status 0))
+    (ignore-errors
+      (setq my-status (server-eval-at server-name (+ 1 1))))
+    (if (/= my-status 2)
+	(progn
+	  (message "Killing stale socket, starting server.")
+	  (server-force-delete)
+	  (server-start))
+      (message "Other server works."))))
+ (t (message "Server already started.")))
 
 (setq bookmark-default-file (concat emacs-cache-dir "bookmarks"))
 (setq backup-directory-alist
