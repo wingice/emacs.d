c08f241124a3748dba65573a646b4dbabeb39eb2
Li <william.li02@sap.com>
2024-03-11 16:11:35 +0800

    Add more host-specific config

--- lisp/local-conf-body.el
+++ lisp/local-conf-body.el
@@ -1,8 +1,6 @@
 ;;----------------- Reference for Per Computer Customization------------------------------
 ;;--- Windows ---
-(when (string= (system-name) "w-PF44DJL0")     ;;T15G2
-  (set-frame-font "Fira Code-12")
-
+(when (eq system-type 'windows-nt)
   (add-to-list 'exec-path "c:/tools/msys64/usr/bin")
   (setenv "PATH" (concat  "c:/tools/msys64/usr/bin;" (getenv "PATH")))
 
@@ -11,4 +9,17 @@
 
   (setenv "P4CLIENT" "pcode3230")
   (setenv "P4PORT" "perforce3230:3230")
-  )
+)
+
+
+(when (or (string= (system-name) "w-PF44DJL0"  )         ;;<--T15g2
+	  (string= (system-name) "W-8CC3372YJW"))        ;;<--HPG800
+  ;;(set-frame-font "Fira Code-12")
+)
+
+(when (string= (system-name) "w-PF44DJL0")         ;;<--T15g2
+  (set-frame-font "Fira Code-12")
+)
+
+
+;;--- Linux/MacOS ---
